2fc779b23a37efc2ba7d80f8c17db8ff2e6ea8b0
alex <alex@alexs-MacBook.local>
2016-11-11 18:11:26 -0500

    init

---
+++ PixDiary/PixDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PixDiary
+//
+//  Created by alex on 11/11/16.
+//  Copyright © 2016 pixcorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
