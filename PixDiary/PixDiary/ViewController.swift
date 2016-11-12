ca1f1af7ad863ab8cbd776802b904c42654c8e36
Hadi Zayer <hmzayer@hotmail.com>
2016-11-11 22:29:28 -0500

    0.01 version

--- PixDiary/PixDiary/ViewController.swift
+++ PixDiary/PixDiary/ViewController.swift
@@ -1,25 +1,43 @@
 //
 //  ViewController.swift
-//  PixDiary
+//  Pixelz Diary
 //
-//  Created by alex on 11/11/16.
-//  Copyright © 2016 pixcorp. All rights reserved.
+//  Created by Hadi Zayer on 11/11/16.
+//  Copyright © 2016 Hadi Zayer. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func load(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImg = info[UIImagePickerControllerOriginalImage] as! UIImage?{
+            imageView.contentMode = .scaleToFill
+            imageView.image = pickedImg
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
